ea41b1a86219b89fd830990b04d278b70791a067
BrunoSSantana <brunoosouza15@gmail.com>
2023-01-20 07:14:44 -0300

    feat: add cors

--- src/http.ts
+++ src/http.ts
@@ -1,15 +1,24 @@
-import 'reflect-metadata'
-import express from 'express'
-import path from 'path'
-import { createServer } from 'http'
-import { Server } from 'socket.io'
+import "reflect-metadata";
+import express from "express";
+import path from "path";
+import { createServer } from "http";
+import { Server } from "socket.io";
+import cors from "cors";
 
-const app = express()
-const server = createServer(app)
+const app = express();
+const server = createServer(app);
 
-app.use(express.static(path.join(__dirname, '..', 'public')))
+app.use(cors({ origin: "*" }));
 
-const io = new Server(server)
-io.on("connection", socket => console.log('Socket Id:', socket.id))
+app.use(express.static(path.join(__dirname, "..", "public")));
 
-export { server, io }
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"]
+  }
+});
+
+io.on("connection", (socket) => console.log("Socket Id:", socket.id));
+
+export { server, io };
